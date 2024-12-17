b4b7a4813fd570727d6385ba1dc5b9b37db8dbb8
Zachary Yedidia <zyedidia@gmail.com>
2024-12-16 20:29:14 -0800

    Add x86 lfi support

--- pal/lfi/arch/x64/regs.c
+++ pal/lfi/arch/x64/regs.c
@@ -5,6 +5,7 @@ void
 pal_regs_init(struct TuxRegs* regs, struct PlatAddrSpace* as, struct PlatContext* ctx)
 {
     regs->r14 = as->base;
-    regs->r13 = ctx->sys;
+    regs->gs  = as->base;
     regs->rsp = as->base;
+    regs->r13 = (uintptr_t) ctx->sys;
 }
