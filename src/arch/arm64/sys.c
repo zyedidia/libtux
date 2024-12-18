6839a767c8222e4b6e89a95506ea37d5928c4038
Zachary Yedidia <zyedidia@gmail.com>
2024-12-17 21:36:23 -0500

    Fix arm64

--- src/arch/arm64/sys.c
+++ src/arch/arm64/sys.c
@@ -11,13 +11,13 @@
 void
 arch_syshandle(struct PlatContext* ctx)
 {
-    struct TuxProc* proc = (struct TuxProc*) pal_ctx_data(ctx);
+    struct TuxThread* p = (struct TuxThread*) pal_ctx_data(ctx);
     struct TuxRegs* regs = pal_ctx_regs(ctx);
 
     switch (regs->x8) {
     default:
         // Generic syscalls.
-        regs->x0 = syshandle(proc, regs->x8, regs->x0, regs->x1, regs->x2,
+        regs->x0 = syshandle(p, regs->x8, regs->x0, regs->x1, regs->x2,
                 regs->x3, regs->x4, regs->x5);
     }
 }
