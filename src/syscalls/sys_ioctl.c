df4c478818bac69adc729b1edaae6e45c11da0a1
Zachary Yedidia <zyedidia@gmail.com>
2025-01-16 21:15:59 +0000

    Fix ioctl file descriptors

--- src/syscalls/sys_ioctl.c
+++ src/syscalls/sys_ioctl.c
@@ -17,7 +17,7 @@ int
 sys_ioctl(struct TuxProc* p, int fd, unsigned long request, uintptr_t va0,
         uintptr_t va1, uintptr_t va2, uintptr_t va3)
 {
-    if (fd != 1)
+    if (fd != 0 && fd != 1 && fd != 2)
         return -TUX_EINVAL;
     if (request != TUX_TIOCGWINSZ) {
         WARN("unknown ioctl request: %ld", request);
@@ -29,9 +29,11 @@ sys_ioctl(struct TuxProc* p, int fd, unsigned long request, uintptr_t va0,
     struct WinSize* tux_ws = (struct WinSize*) wsb;
     struct winsize ws;
     // TODO: use host call for this
-    int err = ioctl(fileno(stdin), TIOCGWINSZ, &ws);
-    if (err != 0)
+    int kfd = fd == 0 ? fileno(stdin) : fd == 1 ? fileno(stdout) : fileno(stderr);
+    int err = ioctl(kfd, TIOCGWINSZ, &ws);
+    if (err != 0) {
         return -errno;
+    }
     tux_ws->ws_row = ws.ws_row;
     tux_ws->ws_col = ws.ws_col;
     tux_ws->ws_xpixel = ws.ws_xpixel;
